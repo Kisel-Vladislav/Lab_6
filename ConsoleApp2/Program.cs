d48f479034dbcc785d041239f1d722e8ee9c437e
vladk <vladk@DESKTOP-I1M9HA6>
2023-03-10 14:57:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 3, denominator = 4, read1 = 3, read2 = 7;
+            string path = "C:/Users/vladk/OneDrive/Рабочий стол/C#/Lab_6/lab1.dat";
+            string path2 = "C:/Users/vladk/OneDrive/Рабочий стол/C#/Lab_6/lab1(2).dat";
+
+            BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate));
+            for (int i = 1; i < 10; i++)
+            {
+                writer.Write(first);
+                Console.WriteLine("В файл 1 записали: " + first + '[' + i + ']');
+                first *= denominator;
+            }
+            writer.Close();
+
+            BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
+            BinaryWriter writer2 = new BinaryWriter(File.Open(path2, FileMode.OpenOrCreate));
+            for (int i = 1; reader.PeekChar() != -1; i++)
+            {
+                int a = 0;
+                if (i == read1 || i == read2)
+                {
+                    //writer2.Write(reader.ReadInt32());
+                    a = reader.ReadInt32();
+                    writer2.Write(a);
+                    Console.WriteLine("\nВ файл 2 записали: " + a + '[' + i + ']');
+                }
+                else
+                    reader.ReadInt32();
+            }
+            reader.Close();
+            writer2.Close();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countReplace = 0;
+            string text = File.ReadAllText("C:\\Users\\vladk\\OneDrive\\Рабочий стол\\C#\\Lab_6\\text.txt");
+            char[] replace = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < replace.Length; j++)
+                {
+                    if (text[i] == replace[j])
+                    {
+                        countReplace++;
+                    }
+                }   
+            }
+            
+            for (int i = 0; i < replace.Length; i++)
+            {
+                text = text.Replace(replace[i],'*');
+            }
+            File.WriteAllText("C:\\Users\\vladk\\OneDrive\\Рабочий стол\\C#\\Lab_6\\text1.txt",text);
+
+            Console.WriteLine("Количество замен: " + countReplace);
+        }
+    }
+}
